168ea460a0d9d092f5c013fca801193480c095ea
atocqueville <alex.detocqueville@gmail.com>
2018-01-17 00:38:04 +0100

    refacto update candle

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
 const WebSocket = require('ws');
+const wsCandles = new WebSocket('wss://api.bitfinex.com/ws/2');
+
 let apiKeys = require('./apikeys.json');
+const keyPublic = apiKeys.public;
+const keySecret = apiKeys.private;
+
+let config = require('./config.json');
+const period = config.period;
+const MTS = config.timestamp;
+const currency = config.currency;
 
 let couchbase = require('couchbase');
 let cluster = new couchbase.Cluster('127.0.0.1');
 cluster.authenticate('Tokie', 'detoka');
 
-const wsCandles = new WebSocket('wss://api.bitfinex.com/ws/2');
 let candlesJSON = [];
-let period = 9;
-let MTS = 1;
+
 let lastRSI;
 let long = false;
 let buy;
@@ -17,8 +24,6 @@ let sell;
 wsCandles.onopen = () => {
   let count = 0;
   const crypto = require('crypto-js');
-  const keyPublic = apiKeys.public;
-  const keySecret = apiKeys.private;
 
   const authNonce = Date.now() * 1000;
   const authPayload = 'AUTH' + authNonce;
@@ -39,7 +44,7 @@ wsCandles.onopen = () => {
   let msg = ({
     'event': 'subscribe',
     'channel': 'candles',
-    'key' : 'trade:' + MTS.toString() + 'm:tBTCUSD'
+    'key': 'trade:' + MTS + 'm:t' + currency + 'USD'
   });
 
   // te = trade executed
@@ -50,16 +55,16 @@ wsCandles.onopen = () => {
   wsCandles.onmessage = (msg) => {
     let response = JSON.parse(msg.data);
     if (response[1] === 'hb') {
-     // console.log('heartbeat');
+      // console.log('heartbeat');
     } else {
       if (count < 3) {
         if (count === 2) {
           let previousCandles = response[1];
-          initCouchBase(previousCandles, period);
+          initCouchbase(previousCandles, period);
         }
         count++;
       } else {
-        // updateJSON(response[1]);
+        retrieveDocument(response[1]);
       }
     }
   }
@@ -84,7 +89,7 @@ function makeDecisions(candleJSON) {
   }
 }
 
-function initCouchBase(previousCandles, period) {
+function initCouchbase(previousCandles, period) {
   let bucket = cluster.openBucket('candles', function(err) {
     if (err) {
       console.log('cant open bucket');
@@ -96,29 +101,30 @@ function initCouchBase(previousCandles, period) {
     bucket.upsert(new Date().toLocaleDateString(),
       candlesJSON
       , function(err, result) {
-      if (!err) {
-        console.log("stored document successfully. CAS is %j", result.cas);
-      } else {
-        console.error("Couldn't store document: %j", err);
-      }
-    });
-
+        if (!err) {
+          console.log("stored document successfully. CAS is %j", result.cas);
+        } else {
+          console.error("Couldn't store document: %j", err);
+        }
+      });
   });
-
 }
 
-function initJSON(previousCandles, period){
+function initJSON(previousCandles, period) {
   previousCandles.reverse();
 
   for (let i = 1; i < previousCandles.length; i++) {
     candlesJSON.push(
       {
         MTS: previousCandles[i][0],
-        CLOSE: previousCandles[i][2],
-        DIFF: previousCandles[i][2] - previousCandles[i-1][2],
-        AVGGAIN: '',
-        AVGLOSS: '',
-        RSI: ''
+        DATA:
+          {
+            CLOSE: previousCandles[i][2],
+            DIFF: previousCandles[i][2] - previousCandles[i - 1][2],
+            AVGGAIN: '',
+            AVGLOSS: '',
+            RSI: ''
+          }
       }
     );
   }
@@ -126,7 +132,7 @@ function initJSON(previousCandles, period){
   let avgGain = 0;
   let avgLoss = 0;
   for (let i = 1; i < period + 1; i++) {
-    let candle = candlesJSON[i];
+    let candle = candlesJSON[i].DATA;
     if (candle.DIFF > 0) {
       avgGain += candle.DIFF;
     } else {
@@ -138,8 +144,8 @@ function initJSON(previousCandles, period){
   firstDynamicCandle.AVGLOSS = avgLoss / period;
 
   for (let i = period + 1; i < previousCandles.length - 1; i++) {
-    let previousCandle = candlesJSON[i-1];
-    let candle = candlesJSON[i];
+    let previousCandle = candlesJSON[i - 1].DATA;
+    let candle = candlesJSON[i].DATA;
     let previousAvgGain = previousCandle.AVGGAIN;
     let previousAvgLoss = previousCandle.AVGLOSS;
     let diff = candle.DIFF;
@@ -158,31 +164,103 @@ function initJSON(previousCandles, period){
   }
 }
 
-function updateJSON(response) {
-  let candleJSON = candlesJSON.find({
-    'MTS': response[0]
-  });
-  if (candleJSON.length === 0) {
-    createCandle(response);
-  } else {
-    if (candleJSON[0].CLOSE !== response[2]) {
-      updateCandle(candleJSON, response);
-      makeDecisions(candleJSON);
+function retrieveDocument(response) {
+  let bucket = cluster.openBucket('candles', function (err) {
+    if (err) {
+      console.log('cant open bucket');
+      throw err;
     }
-  }
+
+    bucket.get('2018-1-17', function (err, result) {
+      if (err) {
+        console.log('Some error occurred: %j', err);
+      } else {
+        let documentCB = result.value;
+        updateJSON(response, documentCB);
+      }
+    })
+  });
 }
 
-function createCandle(candleBitfinex) {
-  let previousCandle = candlesJSON.find({
-    'MTS': candleBitfinex[0] - (60000 * Number(MTS))
-  });
-  candlesJSON.insert({
-    MTS: candleBitfinex[0],
-    CLOSE: candleBitfinex[2],
-    DIFF: candleBitfinex[2] - previousCandle[0].CLOSE,
-    AVGGAIN: '',
-    AVGLOSS: '',
-    RSI: ''
+function getObjects(obj, key, val) {
+  let objects = [];
+  for (let i in obj) {
+    if (!obj.hasOwnProperty(i)) continue;
+    if (typeof obj[i] == 'object') {
+      objects = objects.concat(getObjects(obj[i], key, val));
+    } else
+    //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
+    if (i == key && obj[i] == val || i == key && val == '') { //
+      objects.push(obj);
+    } else if (obj[i] == val && key == '') {
+      //only add if the object is not already in the array
+      if (objects.lastIndexOf(obj) == -1) {
+        objects.push(obj);
+      }
+    }
+  }
+  return objects;
+}
+
+function updateJSON(response, documentCB) {
+
+  let search = getObjects(documentCB, 'MTS', response[0]);
+
+  console.log(search);
+
+  if (search.length !== 0) {
+    console.log('existe !');
+    // if (candleJSON[0].CLOSE !== response[2]) {
+    //   updateCandle(candleJSON, response);
+    //   makeDecisions(candleJSON);
+    // }
+  } else {
+    console.log('existe pas !');
+    createCandle(response, documentCB);
+  }
+
+
+  // let isExist = false;
+  //
+  // for (let i = 0; i < documentCB.length; i++) {
+  //   if (documentCB[i].MTS === response[0]) {
+  //     isExist = true;
+  //     let candle = documentCB[i].DATA;
+  //   }
+  // }
+}
+
+function createCandle(candleBitfinex, documentCB) {
+
+  let previousCandle = getObjects(documentCB, 'MTS', candleBitfinex[0] - (60000 * Number(MTS)))[0].DATA;
+
+  documentCB.push(
+    {
+      MTS: candleBitfinex[0],
+      DATA:
+        {
+          CLOSE: candleBitfinex[2],
+          DIFF: candleBitfinex[2] - previousCandle.CLOSE,
+          AVGGAIN: '',
+          AVGLOSS: '',
+          RSI: ''
+        }
+    }
+  );
+
+  let bucket = cluster.openBucket('candles', function (err) {
+    if (err) {
+      console.log('cant open bucket');
+      throw err;
+    }
+
+    bucket.upsert('2018-1-17', documentCB, function (err, result) {
+      if (!err) {
+        console.log("stored document successfully. CAS is %j", result.cas);
+      } else {
+        console.error("Couldn't store document: %j", err);
+      }
+    });
   });
 }
 
